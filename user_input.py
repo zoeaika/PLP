9a95c937b453c686aa2852737a1ef7b4dae521f6
Zoe Saudi <saudizoe@gmail.com>
2024-03-08 10:07:47 +0300

    first assignement

---
+++ user_input.py
@@ -0,0 +1,13 @@
+name = input("what is your name?")
+
+print('My name is', name)
+
+age = input("what is your age?")
+
+print('I am', age)
+
+location = input("Where are you located?")
+
+print('My location is', location)
+
+print(f'Hello {name}, you are {age} years old and live in {location}')
